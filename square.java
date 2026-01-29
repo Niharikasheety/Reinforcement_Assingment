35f7f67765eb79ef2cadffb22fc31c9a17ee9d4f
Niharika_Shetty <niharikarb191@gmail.com>
2026-01-30 05:25:59 +0530

    Add AreaOfSquare class to calculate square area

---
+++ square.java
@@ -0,0 +1,8 @@
+class AreaOfSquare {
+    public static void main(String[] args) {
+        int side = 5;   // length of side
+        int area = side * side;
+
+        System.out.println("Area of Square = " + area);
+    }
+}
